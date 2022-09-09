30f94ca07550f957551b86de6b1aabb68f402c01
Rayan <rayanchambet1@gmail.com>
2022-09-09 17:09:45 +0200

    V.0.3
    Header ✅
    Nav ✅
    About ✅
    Vision ✅

---
+++ src/components/SectionVision/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './SectionVision.scss'
+
+const SectionVision = () => {
+
+    return(
+        <section className="container__vision">
+            <div className="container__vision-title">
+                    <span className="line"></span>
+                    <div>
+                        <h2>Our Vision.</h2>
+                        <span>AI IS THE NEW DOOR FOR SUCCESS</span>
+                    </div>
+             </div>
+             <div className="box-vision">
+                <p>
+                Artificial intelligence is the new fuel. As petrol revolutionized the world and ushered in the Second Industrial Revolution, so AI is poised to radically transform the economy and the world. We are proud to support entrepreneurs in building companies that move humanity forward by harnessing this power.
+                </p>
+                <p>
+                Important new businesses need to be created to impact difficult problems in a variety of areas. We’re open to all AI applications, but some sectors we’re particularly excited about are human capital, Gaming and wellness, next-generation enterprise.
+                </p>
+             </div>
+        </section>
+    )
+}
+export default SectionVision;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SectionHome from "../../components/SectionHome";
 import SectionAbout from "../../components/SectionAbout";
+import SectionVision from "../../components/SectionVision";
 const Home = () => {
 
     return(
         <div  >
             <SectionHome />    
             <SectionAbout />
+            <SectionVision />
         </div>
     )
 }
